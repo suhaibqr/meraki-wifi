01daaca7fa5de2ae8596a772434e7bb89e2e61bc
skr.jor@gmail.com <skr.jor@gmail.com>
2024-04-16 15:21:17 +0000

    Created LICENSE.txt; Edited settings, MerakiWifi and Form1

--- client_code/MerakiWifi/__init__.py
+++ client_code/MerakiWifi/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import MerakiWifiTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 
 class MerakiWifi(MerakiWifiTemplate):
@@ -11,10 +14,24 @@ class MerakiWifi(MerakiWifiTemplate):
 
   def generate_new_psk_click(self, **event_args):
     """This method is called when the button is clicked"""
-    anvil.server.call('generate_new_psk', self.min_len, self.max_len, self.pass_words)
+    psk, qr = anvil.server.call('generate_new_psk', self.min_len.text, self.max_len.text, self.pass_words.text)
+    self.qr_png.source = qr
+    self.qr_png.visible = True
+    self.network_name.text = "Network Name: TDMDev"
+    self.network_password.text = f"Password is {psk}"
+    self.network_password.visible = True
+    self.network_name.visible = True
     pass
 
   def get_current_psk_click(self, **event_args):
     """This method is called when the button is clicked"""
-    anvil.server.call('get_current_psk')
+
+    psk, qr = anvil.server.call('get_current_psk')
+    # my_media = anvil.BlobMedia(content_type="image/png", content = qr, name= "qr.png" )
+    self.qr_png.source = qr
+    self.qr_png.visible = True
+    self.network_name.text = "Network Name: TDMDev"
+    self.network_password.text = f"Password is: {psk}"
+    self.network_password.visible = True
+    self.network_name.visible = True
     pass
